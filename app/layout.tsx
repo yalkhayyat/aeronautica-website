aea2447e75c2273171ca24f651920426120954da
Yousif Alkhayyat <yalkhayy@gmail.com>
2024-10-01 00:55:27 -0500

    moved home page into correct route, removed from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/app/globals.css";
 
 import NavBar from "@/components/navbar";
-import Home from "@/components/home";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
@@ -15,7 +14,6 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <NavBar />
-          <Home />
           {children}
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import HomePage from "@/components/home";
+
 export default function Home() {
-  return <div></div>;
+  return <HomePage />;
 }
